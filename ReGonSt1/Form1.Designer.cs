04bc7e0406069144d5d4e87411978160deea1ecd
최부건 <cmsboo1482@gmail.com>
2024-05-28 01:21:37 +0900

    version 0.0.5b

--- ReGonSt1/Form1.Designer.cs
+++ ReGonSt1/Form1.Designer.cs
@@ -38,15 +38,14 @@
             this.panel_Home = new System.Windows.Forms.Panel();
             this.panel_Settings = new System.Windows.Forms.Panel();
             this.panel_Help = new System.Windows.Forms.Panel();
-            this.panel_About = new System.Windows.Forms.Panel();
             this.panel_Menu = new System.Windows.Forms.Panel();
+            this.sidebarTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.button_Menu = new System.Windows.Forms.Button();
-            this.button_About = new System.Windows.Forms.Button();
             this.button_Help = new System.Windows.Forms.Button();
             this.button_Settings = new System.Windows.Forms.Button();
             this.button_Home = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.sidebarTimer = new System.Windows.Forms.Timer(this.components);
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.sidebar.SuspendLayout();
@@ -54,14 +53,15 @@
             this.panel_Home.SuspendLayout();
             this.panel_Settings.SuspendLayout();
             this.panel_Help.SuspendLayout();
-            this.panel_About.SuspendLayout();
             this.panel_Menu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.LightGray;
+            this.panel1.Controls.Add(this.pictureBox2);
             this.panel1.Controls.Add(this.labelName);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
@@ -83,8 +83,6 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.Color.LightGray;
-            this.panel2.Controls.Add(this.panel_Menu);
-            this.panel2.Controls.Add(this.panel_About);
             this.panel2.Controls.Add(this.sidebar);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel2.Location = new System.Drawing.Point(0, 68);
@@ -101,17 +99,18 @@
             this.sidebar.MaximumSize = new System.Drawing.Size(269, 613);
             this.sidebar.MinimumSize = new System.Drawing.Size(57, 613);
             this.sidebar.Name = "sidebar";
-            this.sidebar.Size = new System.Drawing.Size(57, 613);
+            this.sidebar.Size = new System.Drawing.Size(269, 613);
             this.sidebar.TabIndex = 0;
             // 
             // panel3
             // 
+            this.panel3.Controls.Add(this.panel_Menu);
             this.panel3.Controls.Add(this.panel_Help);
             this.panel3.Controls.Add(this.panel_Settings);
             this.panel3.Controls.Add(this.panel_Home);
             this.panel3.Location = new System.Drawing.Point(3, 3);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(266, 340);
+            this.panel3.Size = new System.Drawing.Size(389, 338);
             this.panel3.TabIndex = 0;
             // 
             // panel_Home
@@ -139,15 +138,6 @@
             this.panel_Help.Size = new System.Drawing.Size(266, 62);
             this.panel_Help.TabIndex = 1;
             // 
-            // panel_About
-            // 
-            this.panel_About.BackColor = System.Drawing.Color.Silver;
-            this.panel_About.Controls.Add(this.button_About);
-            this.panel_About.Location = new System.Drawing.Point(3, 273);
-            this.panel_About.Name = "panel_About";
-            this.panel_About.Size = new System.Drawing.Size(266, 62);
-            this.panel_About.TabIndex = 1;
-            // 
             // panel_Menu
             // 
             this.panel_Menu.BackColor = System.Drawing.Color.Silver;
@@ -155,10 +145,25 @@
             this.panel_Menu.Location = new System.Drawing.Point(0, 0);
             this.panel_Menu.Name = "panel_Menu";
             this.panel_Menu.Size = new System.Drawing.Size(266, 90);
-            this.panel_Menu.TabIndex = 2;
+            this.panel_Menu.TabIndex = 1;
+            // 
+            // sidebarTimer
+            // 
+            this.sidebarTimer.Tick += new System.EventHandler(this.sidebarTimer_tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = global::ReGonSt1.Properties.Resources.about1;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(991, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(35, 35);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
             // 
             // button_Menu
             // 
+            this.button_Menu.BackColor = System.Drawing.Color.Silver;
             this.button_Menu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button_Menu.Font = new System.Drawing.Font("Segoe Print", 21.75F);
             this.button_Menu.ForeColor = System.Drawing.SystemColors.ControlText;
@@ -171,24 +176,8 @@
             this.button_Menu.TabIndex = 3;
             this.button_Menu.Text = "   Menu";
             this.button_Menu.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button_Menu.UseVisualStyleBackColor = true;
+            this.button_Menu.UseVisualStyleBackColor = false;
             this.button_Menu.Click += new System.EventHandler(this.button_Menu_Click);
-            // 
-            // button_About
-            // 
-            this.button_About.BackColor = System.Drawing.Color.Silver;
-            this.button_About.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button_About.Font = new System.Drawing.Font("Segoe Print", 21.75F);
-            this.button_About.Image = global::ReGonSt1.Properties.Resources.about;
-            this.button_About.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button_About.Location = new System.Drawing.Point(-16, -9);
-            this.button_About.Name = "button_About";
-            this.button_About.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
-            this.button_About.Size = new System.Drawing.Size(300, 78);
-            this.button_About.TabIndex = 2;
-            this.button_About.Text = "   About";
-            this.button_About.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button_About.UseVisualStyleBackColor = false;
             // 
             // button_Help
             // 
@@ -246,10 +235,6 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // sidebarTimer
-            // 
-            this.sidebarTimer.Tick += new System.EventHandler(this.sidebarTimer_tick);
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
@@ -270,8 +255,8 @@
             this.panel_Home.ResumeLayout(false);
             this.panel_Settings.ResumeLayout(false);
             this.panel_Help.ResumeLayout(false);
-            this.panel_About.ResumeLayout(false);
             this.panel_Menu.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -291,11 +276,10 @@
         private System.Windows.Forms.Button button_Settings;
         private System.Windows.Forms.Panel panel_Help;
         private System.Windows.Forms.Button button_Help;
-        private System.Windows.Forms.Panel panel_About;
-        private System.Windows.Forms.Button button_About;
         private System.Windows.Forms.Panel panel_Menu;
         private System.Windows.Forms.Button button_Menu;
         private System.Windows.Forms.Timer sidebarTimer;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- ReGonSt1/Properties/Resources.Designer.cs
+++ ReGonSt1/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace ReGonSt1.Properties {
         /// <summary>
         ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
         /// </summary>
+        internal static System.Drawing.Bitmap about1 {
+            get {
+                object obj = ResourceManager.GetObject("about1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   System.Drawing.Bitmap 형식의 지역화된 리소스를 찾습니다.
+        /// </summary>
         internal static System.Drawing.Bitmap calculator {
             get {
                 object obj = ResourceManager.GetObject("calculator", resourceCulture);
